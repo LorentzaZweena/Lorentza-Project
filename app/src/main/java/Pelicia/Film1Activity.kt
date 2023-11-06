76dbd09a778d963517cd395994d3434c2f8edd8d
Lorentza Coulombia <ariva02zweena@gmail.com>
2023-11-06 20:22:32 +0700

    *sigh

---
+++ app/src/main/java/Pelicia/Film1Activity.kt
@@ -0,0 +1,12 @@
+package Pelicia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lorentza.R
+
+class Film1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_film1)
+    }
+}
