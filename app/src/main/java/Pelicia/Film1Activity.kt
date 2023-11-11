6a93b7a57f4c0aa40557a1d8f1fa7061624e02ad
Lorentza Coulombia <ariva02zweena@gmail.com>
2023-11-11 19:16:15 +0700

    *sigh

--- app/src/main/java/Pelicia/Film1Activity.kt
+++ app/src/main/java/Pelicia/Film1Activity.kt
@@ -1,12 +1,22 @@
 package Pelicia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.example.lorentza.R
+import com.example.lorentza.suround.HomeActivity
 
 class Film1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_film1)
+
+        val ibBackPelicia : ImageButton = findViewById(R.id.ibBackPelicia)
+
+        ibBackPelicia.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
