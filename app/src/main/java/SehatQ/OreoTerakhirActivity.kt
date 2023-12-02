93ab83e5ae8f7b2604a33991c0d18a1bd2c14d0c
Lorentza Coulombia <ariva02zweena@gmail.com>
2023-12-02 17:13:50 +0700

    Another project

--- app/src/main/java/SehatQ/OreoTerakhirActivity.kt
+++ app/src/main/java/SehatQ/OreoTerakhirActivity.kt
@@ -1,12 +1,21 @@
 package SehatQ
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.lorentza.R
 
 class OreoTerakhirActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_oreo_terakhir)
+
+        val btnNext : Button = findViewById(R.id.btnNext)
+
+        btnNext.setOnClickListener {
+            val intent = Intent(this, LogActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
