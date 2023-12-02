a228826d1898869f5d9a231e24e92877055b9d22
Lorentza Coulombia <ariva02zweena@gmail.com>
2023-12-02 15:29:45 +0700

    Mobile programming

---
+++ app/src/main/java/SehatQ/OreoTerakhirActivity.kt
@@ -0,0 +1,12 @@
+package SehatQ
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lorentza.R
+
+class OreoTerakhirActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_oreo_terakhir)
+    }
+}
