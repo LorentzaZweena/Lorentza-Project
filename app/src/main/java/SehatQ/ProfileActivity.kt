714dedff8ecaeb3c6af3fe7c7d49bdaf8d27b7c7
Lorentza Coulombia <ariva02zweena@gmail.com>
2023-12-05 19:45:32 +0700

    Not anymore

--- app/src/main/java/SehatQ/ProfileActivity.kt
+++ app/src/main/java/SehatQ/ProfileActivity.kt
@@ -1,60 +1,56 @@
 package SehatQ
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.Toast
 import com.example.lorentza.R
 
 class ProfileActivity : AppCompatActivity() {
 
-    var ibShareSehatQ : ImageButton? = null
+//    var ibShareSehatQ: ImageButton? = null
+    var ibBackSehatQ: ImageButton? = null
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-        val ibBackSehatQ : ImageButton = findViewById(R.id.ibBackSehatQ)
-        val ibLike : ImageButton = findViewById(R.id.ibLike)
-        val ibLike2 : ImageButton = findViewById(R.id.ibLike2)
+        val ibShareSehatQ: ImageButton= findViewById(R.id.ibShareShehatQ)
+        ibBackSehatQ = findViewById(R.id.ibBackSehatQ)
 
-        ibShareSehatQ = findViewById(R.id.ibShareShehatQ)
-
-        ibBackSehatQ.setOnClickListener {
-            val intent = Intent(this, DashActivity::class.java)
-            startActivity(intent)
-        }
-
-        ibLike.setOnClickListener {
-            if (ibLike2.visibility == View.VISIBLE) {
-                ibLike2.visibility = View.INVISIBLE
-                Toast.makeText(
-                    this, "Ditambahkan ke favorit", Toast.LENGTH_SHORT
-                ).show()
-            }
-            else {
-                ibLike2.visibility = View.VISIBLE
-            }
-        }
-
-        ibShareSehatQ!!.setOnClickListener {
+        ibShareSehatQ.setOnClickListener {
             val intent2 = Intent(Intent.ACTION_SEND)
             intent2.putExtra(Intent.EXTRA_TEXT, "Dokter Isanamikô ")
             intent2.setType("text/plain")
             startActivity(Intent.createChooser(intent, "Bagikan ke : "))
         }
 
-//        ibLike2.setOnClickListener {
-//            if (ibLike.visibility == View.VISIBLE) {
-//                ibLike.visibility = View.INVISIBLE
-//                Toast.makeText(
-//                    this, "Hapus dari favorit", Toast.LENGTH_SHORT
-//                ).show()
-//            }
-//            else {
-//                ibLike.visibility = View.VISIBLE
-//            }
-//        }
+        ibBackSehatQ!!.setOnClickListener {
+            val intent = Intent(this, DashActivity::class.java)
+            startActivity(intent)
+        }
+
+        val ibLike: ImageButton = findViewById(R.id.ibLike)
+        val ivLike: ImageView = findViewById(R.id.ivLike)
+
+        ibLike.setOnClickListener {
+            if (ivLike.visibility == View.VISIBLE) {
+                ivLike.visibility = View.INVISIBLE
+
+                Toast.makeText(
+                    this, "Dihapus dari favorit", Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                ivLike.visibility = View.VISIBLE
+                Toast.makeText(
+                    this, "Ditambahkan ke favorit", Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 }
