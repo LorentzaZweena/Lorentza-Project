0465e671896bae793094e1a91eb2a0bae0e12588
Lorentza Coulombia <ariva02zweena@gmail.com>
2023-12-12 16:11:00 +0700

    Muehehe >:3

--- app/src/main/java/SehatQ/ShopActivity.kt
+++ app/src/main/java/SehatQ/ShopActivity.kt
@@ -1,12 +1,16 @@
 package SehatQ
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lorentza.R
 import com.example.lorentza.suround.adapter.PopularProductAdapter
+import com.example.lorentza.suround.adapterr.RecommendAdapter
 import com.example.lorentza.suround.model.PopularProduct
+import com.example.lorentza.suround.model.Recommend
 
 class ShopActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,5 +32,30 @@ class ShopActivity : AppCompatActivity() {
 
         rvProduct1.adapter = adapter3
         rvProduct1.layoutManager = linearHorizontal3
+
+        val rvProduct2 : RecyclerView = findViewById(R.id.rvProduct2)
+        val Recommend = arrayListOf<Recommend>(
+            Recommend(R.drawable.image104, "Nature made", "Vitamin", "Rp35.000", R.drawable.image105, "Jamieson E", "Vitamin", "Rp23.000"),
+            Recommend(R.drawable.image106, "Actified plus", "Obat", "Rp44.000", R.drawable.image107, "Kandistatin", "Obat", "Rp73.000"),
+            Recommend(R.drawable.image108, "Enfamil", "Vitamin", "Rp75.000", R.drawable.image109, "Mefnic - D", "Obat", "Rp29.000")
+        )
+
+        val adapter4 = RecommendAdapter(Recommend)
+        val linearVertical = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        rvProduct2.adapter = adapter4
+        rvProduct2.layoutManager = linearVertical
+
+        val ibChatSehatQ : ImageButton = findViewById(R.id.ibChatSehatQ)
+        ibChatSehatQ.setOnClickListener {
+            val intent = Intent(this, ChatActivity::class.java)
+            startActivity(intent)
+        }
+
+        val ibShopSehatQ : ImageButton = findViewById(R.id.ibShopSehatQ)
+        ibShopSehatQ.setOnClickListener {
+            val intent = Intent(this, ShopActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
