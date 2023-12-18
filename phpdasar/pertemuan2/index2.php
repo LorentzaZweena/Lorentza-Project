ecdcde6135ce4e635bfd763fd3cf51a50b0c4cc0
Lorentza Coulombia <ariva02zweena@gmail.com>
2023-12-18 13:50:51 +0700

    belajar php

---
+++ phpdasar/pertemuan2/index2.php
@@ -0,0 +1,25 @@
+<?php 
+	// echo "Ariva Zweena";
+	// //ini materi komentar
+	// //dan macam macam output php
+
+	// print "Hello World!";
+	// print_r("PHP dasar");
+	// var_dump("Lorentza");
+
+	// echo 123; //int
+	// echo true; //boolean
+	// echo false; //boolean
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Belajar PHP</title>
+</head>
+<body>
+	<h1>Hello, welcome back <?php echo "Zweena!"; ?></h1>
+	<p><?php echo "ini adalah paragraf"; ?></p>
+</body>
+</html>
