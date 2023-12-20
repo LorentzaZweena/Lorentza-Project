a5855c49360b417a484030071a257a1aaf2e841a
Lorentza Coulombia <ariva02zweena@gmail.com>
2023-12-20 16:24:34 +0700

    belajar php

---
+++ phpdasar/pertemuan6/latihan1.php
@@ -0,0 +1,13 @@
+<?php 
+	//review materi kemaren
+
+	$hari = array("senin","selasa", "rabu");
+	$bulan = ["Januari", "Februari", "Maret"];
+	$arr = [100, "teks", true];
+
+	var_dump($hari);
+	echo "<br>";
+	print_r($bulan);
+
+	echo $arr[0];
+ ?>
