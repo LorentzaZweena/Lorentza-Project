82d64075d52ebf4d207992fccc52b1b0887de8ce
Lorentza Coulombia <ariva02zweena@gmail.com>
2023-12-24 13:51:15 +0700

    belajar php

---
+++ phpdasar/pertemuan7/materi-post.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Post</title>
+</head>
+<body>
+	<form action="materi-post2.php" method="post">
+		Masukkan nama : 
+		<input type="text" name="nama">
+		<br>
+		<button type="submit" name="submit">Submit</button>
+	</form>
+</body>
+</html>
