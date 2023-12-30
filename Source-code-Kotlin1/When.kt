bfe24755dcc0a5399167cd939407ad00dfa877a2
Lorentza Coulombia <ariva02zweena@gmail.com>
2023-12-30 13:21:42 +0700

    Kotlin

---
+++ Source-code-Kotlin1/When.kt
@@ -0,0 +1,11 @@
+fun main(){
+    val finalExam = 'A'
+    when(finalExam){
+        'A' -> println("Very good")
+        'B' -> println("good")
+        'C' -> println("Enough")
+        'D' -> println("Bad")
+        'E' -> println("Very bad")
+        else -> println("You failed")
+    }
+}
