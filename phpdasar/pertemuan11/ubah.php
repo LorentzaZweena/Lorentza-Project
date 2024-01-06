5c063b4e1f5c00a2766da1c44fab553f7cd140d6
Lorentza Coulombia <ariva02zweena@gmail.com>
2024-01-06 15:26:05 +0700

    belajar php

--- phpdasar/pertemuan11/ubah.php
+++ phpdasar/pertemuan11/ubah.php
@@ -6,7 +6,7 @@
 
 	//query data mahasiswa berdasarkan id
 	$mhs = query("SELECT * FROM mahasiswa WHERE id = $id");
-	var_dump($mhs);
+
  ?>
 
 <!DOCTYPE html>
@@ -19,26 +19,27 @@
 <body>
 	<h1>Ubah data mahasiswa</h1>
 	<form action="" method="post">
+		<input type="hidden" name="id" value="<?=$mhs["id"]; ?>">
 		<ul>
 			<li>
 				<label for="nrp">NRP : </label>
-				<input type="text" name="nrp" id="nrp" required>
+				<input type="text" name="nrp" id="nrp" required value="<?= $mhs["nrp"]; ?>">
 			</li>
 			<li>
 				<label for="nrp">Nama : </label>
-				<input type="text" name="nama" id="nama" required>
+				<input type="text" name="nama" id="nama" required value="<?= $mhs["nama"]; ?>">
 			</li>
 			<li>
 				<label for="email">Email : </label>
-				<input type="text" name="email" id="email" required>
+				<input type="text" name="email" id="email" required value="<?= $mhs["email"]; ?>">
 			</li>
 			<li>
 				<label for="jurusan">Jurusan : </label>
-				<input type="text" name="jurusan" id="jurusan" required>
+				<input type="text" name="jurusan" id="jurusan" required value="<?= $mhs["jurusan"]; ?>">
 			</li>
 			<li>
 				<label for="gambar">Gambar : </label>
-				<input type="text" name="gambar" id="gambar" required>
+				<input type="text" name="gambar" id="gambar" required value="<?= $mhs["gambar"]; ?>">
 			</li>
 			<li>
 				<button type="submit" name="submit">Ubah data</button>
