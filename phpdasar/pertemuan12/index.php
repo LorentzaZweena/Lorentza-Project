3f217858222b6c1b25a9370d86096ed96e57a634
Lorentza Coulombia <ariva02zweena@gmail.com>
2024-01-07 15:01:14 +0700

    belajar php

---
+++ phpdasar/pertemuan12/index.php
@@ -0,0 +1,53 @@
+<?php 
+	require 'functions.php';
+	// $mahasiswa = query("SELECT * FROM mahasiswa ORDER BY id DESC"); kalo mau dari yg terlama dulu
+	// $mahasiswa = query("SELECT * FROM mahasiswa ORDER BY id ASC"); kalo mau dai yg terbaru dulu
+	$mahasiswa = query("SELECT * FROM mahasiswa");
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Halaman admin</title>
+	<style>
+		img{
+			width: 20%;
+/*			height: 10%;*/
+		}
+	</style>
+</head>
+<body>
+	<h1>Daftar mahasiswa</h1>
+	<a href="tambah.php">Tambah data mahasiswa</a>
+	<br><br>
+	<table border="1" cellpadding="10" cellspacing="0">
+		<tr>
+			<th>No.</th>
+			<th>Aksi</th>
+			<th>Gambar</th>
+			<th>NRP</th>
+			<th>Nama</th>
+			<th>Email</th>
+			<th>Jurusan</th>
+		</tr>
+		<?php $i = 1; ?>
+		<?php foreach($mahasiswa as $row ): ?>
+		<tr>
+			<td><?= $i; ?></td>
+			<td>
+				<a href="ubah.php?id=<?= $row["id"]; ?>">Ubah</a> |
+				<a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('yakin?');">Hapus</a>
+			</td>
+			<td><img src="img/<?= $row["gambar"]; ?>"></td>
+			<td><?= $row["nrp"]; ?></td>
+			<td><?= $row["nama"]; ?></td>
+			<td><?= $row["email"]; ?></td>
+			<td><?= $row["jurusan"]; ?></td>
+		</tr>
+		<?php $i++; ?>
+	<?php endforeach; ?>
+	</table>
+</body>
+</html>
