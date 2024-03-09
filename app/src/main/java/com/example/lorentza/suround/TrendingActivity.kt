fa07336571a5f05891c857d4d85942b722fa6900
Zweena Ariva <ariva02zweena@gmail.com>
2024-03-09 17:51:12 +0700

    Android

---
+++ app/src/main/java/com/example/lorentza/suround/TrendingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lorentza.suround
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lorentza.R
+
+class TrendingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trending)
+    }
+}
