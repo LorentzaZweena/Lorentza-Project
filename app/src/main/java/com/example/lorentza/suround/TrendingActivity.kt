698058ac9ef55d29b12d9fb9948d36c404da2247
Zweena Ariva <ariva02zweena@gmail.com>
2024-03-10 15:22:00 +0700

    Android project

--- app/src/main/java/com/example/lorentza/suround/TrendingActivity.kt
+++ app/src/main/java/com/example/lorentza/suround/TrendingActivity.kt
@@ -1,12 +1,41 @@
 package com.example.lorentza.suround
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
 import com.example.lorentza.R
 
 class TrendingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trending)
+
+        val ibBackTrending : ImageButton = findViewById(R.id.ibBackTrending)
+        val ibTrendingSuround : ImageButton = findViewById(R.id.ibTrendingSuround)
+        val FavoriteSuround : ImageView = findViewById(R.id.FavoriteSuround)
+
+        ibBackTrending.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        ibTrendingSuround.setOnClickListener {
+            if (FavoriteSuround.visibility == View.VISIBLE) {
+                FavoriteSuround.visibility = View.INVISIBLE
+
+                Toast.makeText(
+                    this, "Removed from favorites", Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                FavoriteSuround.visibility = View.VISIBLE
+                Toast.makeText(
+                    this, "Added from favorites", Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 }
